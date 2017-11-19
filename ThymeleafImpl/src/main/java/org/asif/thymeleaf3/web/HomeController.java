e69f2e19d29d030b9198626eb547016b557b6577
asifaftab87 <asifaftab87@gmail.com>
2017-11-19 15:32:59 +0800

    Perfectly working thymeleaf project

---
+++ ThymeleafImpl/src/main/java/org/asif/thymeleaf3/web/HomeController.java
@@ -0,0 +1,20 @@
+package org.asif.thymeleaf3.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView myModelMethod(Model model){
+    	ModelAndView mav = new ModelAndView("home");
+    	mav.addObject("myName", "Md Asif Aftab");
+    	mav.addObject("myEmail", "asifaftab87@gmail.com");
+    	return mav;
+    }
+
+}
